1a4ec2ef4f3504db26d48285b84dd590e9cbc3af
CarlosR <carlosromero.biz@gmail.com>
2021-11-06 22:46:18 -0700

    Change CSS styles add Delete Function

--- posts/views.py
+++ posts/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from . models import Post
-# Create your views here.
+from . forms import PostForm
 
 
 def index(request):
+    # If the method is POST
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+    # If the form is valid
+        if form.is_valid():
+            # Yes,Save
+            form.save()
+
+            # Redirect to Home
+            return HttpResponseRedirect('/')
+    # No, Show Error
+        else:
+            return HttpResponseRedirect(form.erros.as_json())
+
     # Get all posts, limit = 20
     posts = Post.objects.all()[:20]
 
     # Show
     return render(request, 'posts.html', {'posts': posts})
+
+
+def delete(request, post_id):
+    # Find post
+    post = Post.objects.get(id=post_id)
+    post.delete()
+    return HttpResponseRedirect('/')
